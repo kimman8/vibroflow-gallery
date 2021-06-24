24202e722ef6938b58e77580cf1a6611b467daa8
kimzzles <86180841+kimzzles@users.noreply.github.com>
2021-06-24 22:51:51 +1000

    rounded the search bar

--- src/components/ui/FormCustom.js
+++ src/components/ui/FormCustom.js
@@ -217,7 +217,7 @@ const FormCustom = () => {
                 </a>
                 <input
                   type="text"
-                  className="w-52 focus:outline-none focus:ring-2 focus:ring-blue-600 rounded font-mono shadow-2xl text-center focus:placeholder-blue-300 bg-gray-100"
+                  className="w-52 focus:outline-none focus:ring-2 focus:ring-blue-600 hover:ring-2 hover:bg-blue-100 rounded-full border-2 border-gray-400 font-mono shadow-2xl text-center focus:placeholder-blue-300 bg-gray-100"
                   id="standard-basic"
                   placeholder="Search.."
                   label="Search"

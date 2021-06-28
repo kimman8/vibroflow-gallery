bfcfc345ef6dcb4859f12570361e8c003bfa8c81
Kim <kim_man@live.com>
2021-06-28 12:13:46 +1000

    committing before mergin

--- src/components/ui/FormCustom.js
+++ src/components/ui/FormCustom.js
@@ -217,7 +217,7 @@ const FormCustom = () => {
                 </a>
                 <input
                   type="text"
-                  className="w-52 focus:outline-none focus:ring-2 focus:ring-blue-600 rounded font-mono shadow-2xl text-center focus:placeholder-blue-300 bg-gray-100"
+                  className="w-52 focus:outline-none rounded-full hover:bg-blue-100 border-2 border-gray-200 focus:ring-2 focus:ring-blue-600 font-mono shadow-2xl text-center focus:placeholder-blue-300"
                   id="standard-basic"
                   placeholder="Search.."
                   label="Search"

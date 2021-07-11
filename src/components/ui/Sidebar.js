285f3bbbd7ab0c5c6ffac64554e61f7aff7cb0d5
kim <kim_man@live.com>
2021-07-11 16:40:25 +1000

    fixed some warnings

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -1,50 +1,50 @@
-import React, { Fragment, useState } from "react";
-import { SidebarData } from "./SidebarData";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import { IconContext } from "react-icons/lib";
-import SubMenu from "./SubMenu";
+// import React, { Fragment, useState } from "react";
+// import { SidebarData } from "./SidebarData";
+// import styled from "styled-components";
+// import { Link } from "react-router-dom";
+// import * as FaIcons from "react-icons/fa";
+// import * as AiIcons from "react-icons/ai";
+// import { IconContext } from "react-icons/lib";
+// import SubMenu from "./SubMenu";
 
-const Nav = styled.div`
-  background: #15171c;
-  height: 80px;
-  color: white;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
+// const Nav = styled.div`
+//   background: #15171c;
+//   height: 80px;
+//   color: white;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+// `;
 
-const NavIcon = styled(Link)`
-  margin-left: 2rem;
-  font-size: 2rem;
-  height: 80px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  color: white;
-`;
+// const NavIcon = styled(Link)`
+//   margin-left: 2rem;
+//   font-size: 2rem;
+//   height: 80px;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+//   color: white;
+// `;
 
-const SidebarNav = styled.nav`
-  background: #15171c;
-  width: 250px;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  position: fixed;
-  top: 0;
-  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
-  transition: 350ms;
-  z-index: 10;
-`;
+// const SidebarNav = styled.nav`
+//   background: #15171c;
+//   width: 250px;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   position: fixed;
+//   top: 0;
+//   left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+//   transition: 350ms;
+//   z-index: 10;
+// `;
 
-const SidebarWrap = styled.div`
-  width: 100%;
-`;
+// const SidebarWrap = styled.div`
+//   width: 100%;
+// `;
 
-const Sidebar = () => {
-  return <div></div>;
-};
+// const Sidebar = () => {
+//   return <div></div>;
+// };
 
-export default Sidebar;
+// export default Sidebar;
